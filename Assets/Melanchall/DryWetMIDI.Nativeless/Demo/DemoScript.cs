4e745065d6a2c156f22895669dd56042c16bc441
J-Potes <jpotes.multimedia@gmail.com>
2023-09-15 23:55:56 -0500

    Cambio de Midis en tiempo real

--- Assets/Melanchall/DryWetMIDI.Nativeless/Demo/DemoScript.cs
+++ Assets/Melanchall/DryWetMIDI.Nativeless/Demo/DemoScript.cs
@@ -8,6 +8,8 @@ using Melanchall.DryWetMidi.Interaction;
 using Melanchall.DryWetMidi.Multimedia;
 using Melanchall.DryWetMidi.Standards;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class DemoScript : MonoBehaviour
 {
@@ -37,22 +39,93 @@ public class DemoScript : MonoBehaviour
     private IOutputDevice _outputDevice;
     private Playback _playback;
 
+    public TMP_Dropdown musicDropdown; // Asigna el Dropdown desde el Inspector
+    public string[] midiFilePaths = {Application.dataPath + "/MIDIS/MIDI-3.mid",Application.dataPath + "/MIDIS/mario.mid",Application.dataPath + "/MIDIS/MIDI-2.mid"};
+
+    // string midiFilePath = Application.dataPath + "/MIDIS/MIDI-3.mid";
+    // string midiFilePath2 = Application.dataPath + "/MIDIS/mario.mid";
+    // string midiFilePath3 = Application.dataPath + "/MIDIS/MIDI-2.mid";
+
     private void Start()
     {
-        string midiFilePath=Application.dataPath+"/MIDIS/MIDI-3.mid";
         InitializeOutputDevice();
-        var midiFile = MidiFile.Read(midiFilePath);
+        var midiFile = MidiFile.Read(midiFilePaths[0]);
 
         InitializeFilePlayback(midiFile);
+
+        // Suscribe al evento de cambio de valor del Dropdown
+        // musicDropdown.onValueChanged.AddListener(ChangeMusic);
     }
 
-    void Update(){
-        if (Input.GetKeyDown(KeyCode.P)){
-            StartPlayback();
-        }
-        if (Input.GetKeyDown(KeyCode.O)){
+    public void ButtonStart(){
+        StartPlayback();
+    }
+
+    public void ButtonStop(){
+        StopPlayback();
+    }
+
+    public void ButtonMidi1(){
             StopPlayback();
-        }
+            var midiFile = MidiFile.Read(midiFilePaths[0]);
+            InitializeFilePlayback(midiFile);
+            StartPlayback();
+    }
+
+    public void ButtonMidi2(){
+            StopPlayback();
+            var midiFile = MidiFile.Read(midiFilePaths[1]);
+            InitializeFilePlayback(midiFile);
+            StartPlayback();
+    }
+
+    public void ButtonMidi3(){
+            StopPlayback();
+            var midiFile = MidiFile.Read(midiFilePaths[2]);
+            InitializeFilePlayback(midiFile);
+            StartPlayback();
+    }
+
+    // public void ChangeMusic(int selectedIndex)
+    // {
+    //     // Detiene la reproducción actual
+    //     StopPlayback();
+
+    //     // Carga y reproduce el archivo MIDI seleccionado
+    //     InitializeFilePlayback(midiFilePaths[selectedIndex]);
+    //     StartPlayback();
+    // }
+
+
+    void Update(){
+        // var midiFile;
+        // if (Input.GetKeyDown(KeyCode.P)){
+        //     StartPlayback();
+        // }
+        // if (Input.GetKeyDown(KeyCode.O)){
+        //     StopPlayback();
+        // }
+
+        // if(Input.GetKeyDown(KeyCode.A)){
+        //     StopPlayback();
+        //     var midiFile = MidiFile.Read(midiFilePath);
+        //     InitializeFilePlayback(midiFile);
+        //     StartPlayback();
+        // }
+
+        // if(Input.GetKeyDown(KeyCode.S)){
+        //     StopPlayback();
+        //     var midiFile = MidiFile.Read(midiFilePath2);
+        //     InitializeFilePlayback(midiFile);
+        //     StartPlayback();
+        // }
+
+        // if(Input.GetKeyDown(KeyCode.D)){
+        //     StopPlayback();
+        //     var midiFile = MidiFile.Read(midiFilePath3);
+        //     InitializeFilePlayback(midiFile);
+        //     StartPlayback();
+        // }
     }
 
     private void OnApplicationQuit()
